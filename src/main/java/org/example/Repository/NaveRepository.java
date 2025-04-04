5117c102463189028ef28f7f3bd35cf4b86a3812
Abelsabater <abesabmun@alu.edu.gva.es>
2025-04-04 11:59:28 +0200

    Delete src/main directory

--- src/main/java/org/example/Repository/NaveRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.Repository;
-
-import org.example.Entity.Nave;
-import org.springframework.data.jpa.repository.JpaRepository;
-/**
- * Repositorio de Nave
- * @see org.springframework.data.jpa.repository.JpaRepository **/
-public interface NaveRepository extends JpaRepository<Nave, Long> {
-}
--- src/main/java/org/example/Repository/PlanetaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.Repository;
-
-import org.example.Entity.Planeta;
-import org.springframework.data.jpa.repository.JpaRepository;
-/**
- * Repositorio de Planeta
- * @see org.springframework.data.jpa.repository.JpaRepository **/
-public interface PlanetaRepository extends JpaRepository<Planeta, Long> {
-}
